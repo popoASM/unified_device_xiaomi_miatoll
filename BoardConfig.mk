0405b47ba151ef570e474052eb239f14c7dae982
popoASM <pratyayaborborah@gmail.com>
2024-04-05 20:12:33 +0530

    miatoll: Ignore neverallows
    - temp ;)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -192,6 +192,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2023-05-01
 
 # Sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
